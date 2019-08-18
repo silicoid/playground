92dd0d416123089a096d89eb30ebcdda45cd5c86
Andreas Weigl <andreas.weigl@silicoid.de>
2019-08-18 01:20:59 -0700

    Added graph for growth from year to year.

--- pandas/population_mission/analyze.py
+++ pandas/population_mission/analyze.py
@@ -20,5 +20,10 @@ for gender in df["Gender"].unique():
     else:
         df_clean = df_clean.join(gender_df[f'{gender}_Total'])
 
-graph = df_clean.plot().get_figure()
-graph.savefig("test.pdf")
+df_growth = df_clean.diff()
+
+graph_total = df_clean.plot().get_figure()
+graph_total.savefig("total_population.pdf")
+
+graph_growth = df_growth.dropna().plot().get_figure()
+graph_growth.savefig("growth.pdf")
