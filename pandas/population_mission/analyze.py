50e932bda495404c28e4be98dfb33a28b590c90b
Andreas Weigl <andreas.weigl@silicoid.de>
2019-08-18 13:26:27 -0700

    Added .gitignore and did some code cleanup

--- pandas/population_mission/analyze.py
+++ pandas/population_mission/analyze.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 df = pd.read_csv("Population_Estimates.csv")
-df.set_index("Year",inplace=True)
+df.set_index("Year", inplace=True)
 df.sort_index(inplace=True)
 
 df_clean = pd.DataFrame()
@@ -11,7 +11,7 @@ df_clean = pd.DataFrame()
 for gender in df["Gender"].unique():
     print(gender)
 
-    gender_df = df.copy()[df["Gender"]==gender]
+    gender_df = df.copy()[df["Gender"] == gender]
     gender_df[f'{gender}_Total'] = gender_df["Total"]
 
     if df_clean.empty:
