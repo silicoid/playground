67a263b5abfbd3804c29825b92e2f12d09baaefb
Andreas Weigl <Andreas.Weigl@silicoid.de>
2019-11-06 08:26:03 -0700

    Alarm emulation for windows

---
+++ windows_alarm.py
@@ -0,0 +1,25 @@
+import os
+import time
+import threading
+
+class Alarm (threading.Thread):
+    def __init__ (self, timeout):
+        threading.Thread.__init__ (self)
+        self.timeout = timeout
+        self.setDaemon (True)
+    def run (self):
+        time.sleep (self.timeout)
+        print("Break")
+        os._exit (1)
+
+alarm = Alarm (4)
+alarm.start ()
+time.sleep (2)
+del alarm
+print('yup')
+
+alarm = Alarm (4)
+alarm.start ()
+time.sleep (8)
+del alarm
+print('nope')  # we don't make it this far
